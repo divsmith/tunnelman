ec37d4acd1c3df9f905048d411d64428cfcf7fc9
Parker Smith <parker@parkersmith.us>
2026-03-29 14:44:46 -0600

    resize, add quit option

--- Sources/TunnelMan/StatusBarController.swift
+++ Sources/TunnelMan/StatusBarController.swift
@@ -9,14 +9,17 @@ final class StatusBarController: NSObject {
     private let sessionManager = SessionManager()
 
     override init() {
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
         popover = NSPopover()
         super.init()
 
         if let button = statusItem.button {
-            button.image = NSImage(systemSymbolName: "terminal", accessibilityDescription: "TunnelMan")
+            let image = NSImage(systemSymbolName: "terminal", accessibilityDescription: "TunnelMan")
+            image?.isTemplate = true
+            button.image = image
             button.action = #selector(togglePopover)
             button.target = self
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
         }
 
         popover.contentSize = NSSize(width: 340, height: 460)
@@ -28,11 +31,21 @@ final class StatusBarController: NSObject {
 
     @objc func togglePopover() {
         guard let button = statusItem.button else { return }
-        if popover.isShown {
+        if NSApp.currentEvent?.type == .rightMouseUp {
+            showContextMenu()
+        } else if popover.isShown {
             popover.performClose(nil)
         } else {
             popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
         }
+    }
+
+    private func showContextMenu() {
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Quit TunnelMan", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        statusItem.menu = menu
+        statusItem.button?.performClick(nil)
+        statusItem.menu = nil
     }
 
     func updateIcon(state: TunnelState) {
@@ -44,6 +57,8 @@ final class StatusBarController: NSObject {
         case .error:             symbolName = "exclamationmark.triangle"
         case .needsDevTunnelLogin: symbolName = "person.badge.key"
         }
-        statusItem.button?.image = NSImage(systemSymbolName: symbolName, accessibilityDescription: "TunnelMan")
+        let image = NSImage(systemSymbolName: symbolName, accessibilityDescription: "TunnelMan")
+        image?.isTemplate = true
+        statusItem.button?.image = image
     }
 }
